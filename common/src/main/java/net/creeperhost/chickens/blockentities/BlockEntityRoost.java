e3d9bc18458faf0266263c20931fb4bd44a2ae74
Gigabit101 <thegigabit101@gmail.com>
2022-07-12 17:25:22 +0100

    Roost now functions

--- common/src/main/java/net/creeperhost/chickens/blockentities/BlockEntityRoost.java
+++ common/src/main/java/net/creeperhost/chickens/blockentities/BlockEntityRoost.java
@@ -1,17 +1,26 @@
 package net.creeperhost.chickens.blockentities;
 
+import net.creeperhost.chickens.api.ChickenStats;
+import net.creeperhost.chickens.api.ChickensRegistry;
+import net.creeperhost.chickens.api.ChickensRegistryItem;
 import net.creeperhost.chickens.containers.ContainerRoost;
 import net.creeperhost.chickens.init.ModBlocks;
+import net.creeperhost.chickens.item.ItemChicken;
 import net.creeperhost.chickens.polylib.PolyInventory;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
@@ -62,7 +71,57 @@ public class BlockEntityRoost extends BaseContainerBlockEntity
 
     public void tick()
     {
-        
+        if(level != null && !level.isClientSide)
+        {
+            if (!inventory.getItem(0).isEmpty() && progress <= 1000)
+            {
+                progress++;
+            } else
+            {
+                ChickensRegistryItem chickensRegistryItem = ChickensRegistry.getByRegistryName(ItemChicken.getTypeFromStack(inventory.getItem(0)));
+                if (chickensRegistryItem != null)
+                {
+                    ChickenStats chickenStats = new ChickenStats(inventory.getItem(0));
+                    int gain = chickenStats.getGain();
+                    int chickens = inventory.getItem(0).getCount();
+                    ItemStack itemToLay = chickensRegistryItem.createLayItem();
+                    if (gain >= 5)
+                    {
+                        itemToLay.grow(chickensRegistryItem.createLayItem().getCount());
+                    }
+                    if (gain >= 10)
+                    {
+                        itemToLay.grow(chickensRegistryItem.createLayItem().getCount());
+                    }
+                    int finalCount = itemToLay.getCount() * chickens;
+                    itemToLay.setCount(finalCount);
+                    ItemStack inserted = inventory.addItem(itemToLay);
+                    if(inserted.isEmpty())
+                    {
+                        level.playSound(null, getBlockPos(), SoundEvents.CHICKEN_EGG, SoundSource.NEUTRAL, 0.5F, 0.8F);
+
+                        spawnParticle(level, getBlockPos().getX(), getBlockPos().getY() + 1, getBlockPos().getZ(), level.random);
+                        progress = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void spawnParticle(Level worldIn, double posX, double posY, double posZ, RandomSource rand)
+    {
+        for (int i = 0; i < 16; ++i)
+        {
+            int j = rand.nextInt(2) * 2 - 1;
+            int k = rand.nextInt(2) * 2 - 1;
+            double d0 = posX + 0.5D + 0.25D * (double) j;
+            double d1 = ((float) posY + rand.nextFloat());
+            double d2 = posZ + 0.5D + 0.25D * (double) k;
+            double d3 = (rand.nextFloat() * (float) j);
+            double d4 = (rand.nextFloat() - 0.5D) * 0.125D;
+            double d5 = (rand.nextFloat() * (float) k);
+            worldIn.addParticle(ParticleTypes.HEART, d0, d1, d2, d3, d4, d5);
+        }
     }
 
     @Override
