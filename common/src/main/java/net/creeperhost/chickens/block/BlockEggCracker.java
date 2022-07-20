6f71e0fe959767298de8510111088484a467088c
Gigabit101 <thegigabit101@gmail.com>
2022-07-20 19:50:11 +0100

    Temp use Incubator model for Egg Cracker

--- common/src/main/java/net/creeperhost/chickens/block/BlockEggCracker.java
+++ common/src/main/java/net/creeperhost/chickens/block/BlockEggCracker.java
@@ -3,16 +3,22 @@ package net.creeperhost.chickens.block;
 import dev.architectury.registry.menu.MenuRegistry;
 import net.creeperhost.chickens.blockentities.BlockEntityEggCracker;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
@@ -20,9 +26,24 @@ import org.jetbrains.annotations.Nullable;
 
 public class BlockEggCracker extends BlockBase
 {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
     public BlockEggCracker()
     {
         super(Properties.of(Material.METAL).strength(2.0F));
+        this.registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING);
     }
 
     @Override
